8abb75211b6d68c57228164c21dfb7ab3873a0ec
“EdgarHdz-13” <ie720967@iteso.mx>
2022-02-15 21:30:41 -0600

    Initial version with possible functions

--- RGB.h
+++ RGB.h
@@ -11,11 +11,49 @@
 
 #ifndef RGB_H_
 #define RGB_H_
-
+/*! These constants are used to select an color for the RGB*/
 typedef enum{
-	RED,BLUE,GREEN,PURPLE,YELLOW,CYAN,WHITE
-}rgb_color;
+            RED,
+			BLUE,
+			GREEN,
+			PURPLE,
+			YELLOW,
+			CYAN,
+			WHITE
+            }rgb_color;
 
-void initialize_rgb(rgb_color);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+\brief	 This function clears all interrupts that were sensed by the GPIO.
+
+\param[in]  port_name Port to clear interrupts.
+\return void
+\todo Implement a mechanism to clear interrupts by a specific pin.
+*/
+void initialize_rgb(rgb_color RGB_color_name);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+\brief	 This function clears all interrupts that were sensed by the GPIO.
+
+\param[in]  port_name Port to clear interrupts.
+\return void
+\todo Implement a mechanism to clear interrupts by a specific pin.
+*/
+void turn_on(rgb_color RGB_color_name);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+\brief	 This function clears all interrupts that were sensed by the GPIO.
+
+\param[in]  port_name Port to clear interrupts.
+\return void
+\todo Implement a mechanism to clear interrupts by a specific pin.
+*/
+void turn_off(rgb_color RGB_color_name);
 
 #endif /* RGB_H_ */
