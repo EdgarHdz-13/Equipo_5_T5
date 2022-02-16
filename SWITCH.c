d5579b7ee9cf96ff18d2a13c6b4c486fba101e63
ie722283 <vdani.cg@gmail.com>
2022-02-16 12:54:50 -0600

    added read and initialize switch functions

--- SWITCH.c
+++ SWITCH.c
@@ -4,6 +4,64 @@
  *  Created on: 11 feb 2022
  *      Author: Edgar Hernández, Victor Cervantes
  */
+#include <stdio.h>
+#include "bits.h"
+#include "MK64F12.h"
+#include "GPIO.h"
+#include "SWITCH.h"
 
+static gpio_pin_control_register_t pcr_SW2_pin = GPIO_MUX1;
+static gpio_pin_control_register_t pcr_SW3_pin = GPIO_MUX1;
 
-//TODO: Implement functions
+void SW_initialize_switch(switch_button sw_button, switch_state state)
+{
+	//** Implement switch case to initialize nswitch. Must do clok gating and PCR config
+	switch(sw_button)
+			{
+				case SW2:
+					GPIO_clock_gating(GPIO_C);
+					switch(state)
+					{
+					case PULLUP:
+						GPIO_pin_control_register(GPIO_C,SW2_PIN,&(pcr_SW2_pin&GPIO_PS));
+						break;
+					case PULLDOWN:
+						GPIO_pin_control_register(GPIO_C,SW2_PIN,&(pcr_SW2_pin&GPIO_PE));
+						break;
+					default:
+						break;
+					}
+				break;
+				case SW3:
+					GPIO_clock_gating(GPIO_A);
+					switch(state)
+					{
+					case PULLUP:
+						GPIO_pin_control_register(GPIO_A,SW3_PIN,&(pcr_SW3_pin&GPIO_PS));
+						break;
+					case PULLDOWN:
+						GPIO_pin_control_register(GPIO_A,SW3_PIN,&(pcr_SW3_pin&GPIO_PE));
+						break;
+					default:
+						break;
+					}
+				break;
+				default: /**If doesn't exist the option*/
+				break;
+			}// end switch
+}
+
+uint8_t SW_read_switch_state(switch_button)
+{
+	switch(sw_button)
+			{
+				case SW2:
+					return GPIO_read_pin(GPIO_C, SW2_PIN);
+				break;
+				case SW3:
+					return GPIO_read_pin(GPIO_A, SW3_PIN);
+				break;
+				default: /**If doesn't exist the option*/
+				break;
+			}// end switch
+}
