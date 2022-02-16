fa46965c1ddf0894931104b5353dd54290ba9bbd
“EdgarHdz-13” <ie720967@iteso.mx>
2022-02-16 09:56:49 -0600

    RGB_Initialize and some change to better comprehension

--- RGB.c
+++ RGB.c
@@ -4,5 +4,89 @@
  *  Created on: 11 feb 2022
  *      Author: Edgar Hernández, Victor Cervantes
  */
+#include <stdio.h>
+#include "bits.h"
+#include "MK64F12.h"
+#include "GPIO.h"
+#include "RGB.h"
 
+static gpio_pin_control_register_t pcr_red_pin = GPIO_MUX1;
+static gpio_pin_control_register_t pcr_green_pin = GPIO_MUX1;
+static gpio_pin_control_register_t pcr_blue_pin = GPIO_MUX1;
 
+void RGB_initialize(rgb_color RGB_color_name)
+{
+    switch(RGB_color_name)
+    {
+        case YELLOW:
+            GPIO_clock_gating(GPIO_E);
+            GPIO_pin_control_register(GPIO_E,GREEN_PIN,&pcr_green_pin);
+            GPIO_set_pin(GPIO_E,GREEN_PIN);
+            GPIO_data_direction_pin(GPIO_E,GPIO_OUTPUT,GREEN_PIN);
+        // Also perform the steps for RED
+        case RED:
+            GPIO_clock_gating(GPIO_B);
+            GPIO_pin_control_register(GPIO_B,RED_PIN,&pcr_red_pin);
+            GPIO_set_pin(GPIO_B,RED_PIN);
+            GPIO_data_direction_pin(GPIO_B,GPIO_OUTPUT,RED_PIN);
+        break;
+        case PURPLE:
+            GPIO_clock_gating(GPIO_B);
+            GPIO_pin_control_register(GPIO_B,RED_PIN,&pcr_red_pin);
+            GPIO_set_pin(GPIO_B,RED_PIN);
+            GPIO_data_direction_pin(GPIO_B,GPIO_OUTPUT,RED_PIN);
+        // Also perform the steps for BLUE
+        case BLUE:
+            GPIO_clock_gating(GPIO_B);
+            GPIO_pin_control_register(GPIO_B,BLUE_PIN,&pcr_blue_pin);
+            GPIO_set_pin(GPIO_B,BLUE_PIN);
+            GPIO_data_direction_pin(GPIO_B,GPIO_OUTPUT,BLUE_PIN);
+        break;
+        case WHITE:
+            GPIO_clock_gating(GPIO_B);
+            GPIO_pin_control_register(GPIO_B,RED_PIN,&pcr_red_pin);
+            GPIO_set_pin(GPIO_B,RED_PIN);
+            GPIO_data_direction_pin(GPIO_B,GPIO_OUTPUT,RED_PIN);
+        // Also perform the steps for CYAN and GREEN
+        case CYAN:
+            GPIO_clock_gating(GPIO_B);
+            GPIO_pin_control_register(GPIO_B,BLUE_PIN,&pcr_blue_pin);
+            GPIO_set_pin(GPIO_B,BLUE_PIN);
+            GPIO_data_direction_pin(GPIO_B,GPIO_OUTPUT,BLUE_PIN);
+        // Also perform the steps for GREEN
+        case GREEN:
+            GPIO_clock_gating(GPIO_E);
+            GPIO_pin_control_register(GPIO_E,GREEN_PIN,&pcr_green_pin);
+            GPIO_set_pin(GPIO_E,GREEN_PIN);
+            GPIO_data_direction_pin(GPIO_E,GPIO_OUTPUT,GREEN_PIN);
+        break;
+    }
+}
+
+void RGB_initialize_all()
+{
+
+    GPIO_clock_gating(GPIO_B);
+    GPIO_pin_control_register(GPIO_B,RED_PIN,&pcr_red_pin);
+    GPIO_set_pin(GPIO_B,RED_PIN);
+    GPIO_data_direction_pin(GPIO_B,GPIO_OUTPUT,RED_PIN);
+
+    GPIO_pin_control_register(GPIO_B,BLUE_PIN,&pcr_blue_pin);
+    GPIO_set_pin(GPIO_B,BLUE_PIN);
+    GPIO_data_direction_pin(GPIO_B,GPIO_OUTPUT,BLUE_PIN);
+
+    GPIO_clock_gating(GPIO_E);
+    GPIO_pin_control_register(GPIO_E,GREEN_PIN,&pcr_green_pin);
+    GPIO_set_pin(GPIO_E,GREEN_PIN);
+    GPIO_data_direction_pin(GPIO_E,GPIO_OUTPUT,GREEN_PIN);
+}
+
+void RGB_turn_on(rgb_color RGB_color_name)
+{
+
+}
+
+void RGB_turn_off(rgb_color RGB_color_name)
+{
+
+}
--- RGB.h
+++ RGB.h
@@ -4,13 +4,18 @@
  *  Created on: 11 feb 2022
  *      Author: Edgar Hernández, Victor Cervantes
  */
-#include <stdio.h>
-#include <stdint.h>
-#include "bits.h"
-#include "MK64F12.h"
-
 #ifndef RGB_H_
 #define RGB_H_
+
+#include "stdint.h"
+
+// Constant that represent the pin number where is the red led
+#define RED_PIN 22
+// Constant that represent the pin number where is the red led
+#define BLUE_PIN 21
+// Constant that represent the pin number where is the red led
+#define GREEN_PIN 27
+
 /*! These constants are used to select an color for the RGB*/
 typedef enum{
             RED,
@@ -26,34 +31,43 @@ typedef enum{
 /********************************************************************************************/
 /********************************************************************************************/
 /*!
-\brief	 This function clears all interrupts that were sensed by the GPIO.
+\brief	 This function turn on the clock gating, pin control register and establishes the port as output
+         by default the led is turn off.
 
-\param[in]  port_name Port to clear interrupts.
+\param[in]  RGB_color_name RGB to turn on.
 \return void
-\todo Implement a mechanism to clear interrupts by a specific pin.
+\todo Turn on the clock gating, pin control register, and establishes the port as output
+      by default the led is turn off.
 */
-void initialize_rgb(rgb_color RGB_color_name);
+void RGB_initialize(rgb_color RGB_color_name);
 /********************************************************************************************/
 /********************************************************************************************/
 /********************************************************************************************/
 /*!
-\brief	 This function clears all interrupts that were sensed by the GPIO.
+\brief	 This function turn on all for led's clock gating, pin control register and establishes the ports as output
+         by default the leds is turn off.
 
-\param[in]  port_name Port to clear interrupts.
-\return void
-\todo Implement a mechanism to clear interrupts by a specific pin.
 */
-void turn_on(rgb_color RGB_color_name);
+void RGB_initialize_all();
 /********************************************************************************************/
 /********************************************************************************************/
 /********************************************************************************************/
 /*!
-\brief	 This function clears all interrupts that were sensed by the GPIO.
+\brief	 This function turn on the RGB led/leds for the color selected.
 
-\param[in]  port_name Port to clear interrupts.
+\param[in]  RGB_color_name RGB to turn on.
 \return void
-\todo Implement a mechanism to clear interrupts by a specific pin.
 */
-void turn_off(rgb_color RGB_color_name);
+void RGB_turn_on(rgb_color RGB_color_name);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+\brief	 This function turn off the RGB led/leds for the color selected.
+
+\param[in]  RGB_color_name RGB to turn off.
+\return void
+*/
+void RGB_turn_off(rgb_color RGB_color_name);
 
 #endif /* RGB_H_ */
