d4b2e89dd7f789a7bc3eaacf85779ff6a095be98
ie722283 <vdani.cg@gmail.com>
2022-02-16 19:04:57 -0600

    added coments in switch.h

--- SWITCH.h
+++ SWITCH.h
@@ -4,21 +4,40 @@
 #include "stdint.h"
 #include "bits.h"
 
+/** Constant that represent the pin that corresponds to the seond switch*/
 #define SW2_PIN 6
-
+/** Constant that represent the pin that corresponds to the third switch*/
 #define SW3_PIN 4
 
+/*! These constants are used to select an specific switch in the different API functions*/
 typedef enum{
 	SW2,SW3
 }switch_button;
 
+/*! These constants are used to select an specific switch configuration in the different API functions*/
 typedef enum{
 	PULLUP,PULLDOWN
 }switch_state;
 
 void SW_initialize_switch(switch_button sw_button, switch_state state);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ 	 \brief This function intialize a specifc switch.
+ 	 \param[in]switch_button sw_button to be.
+ 	 \param[in] switch_state state to be set in PCR.
+ 	 \return void
+ */
 
+boolean_t SW_read_switch_state(switch_button sw_button);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ 	 \brief	 This function reads a specific SWITCH state.
+	 \param[in] switch_button sw_button to be read.
+ 	 \return This function the current state of a switch returning a boolean value 0 if it is not pressed and 1 if it is pressed.
+ */
 
-boolean_t SW_read_switch_state(switch_button);
-
-#endif /* DELAY_H_ */
+#endif /* SWITCH_H_ */
